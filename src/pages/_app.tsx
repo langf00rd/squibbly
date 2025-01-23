474abba8ed5940a6f5ba4103dfee5e6591a52788
langford // 🇬🇭 <langfordquarshie21@gmail.com>
2025-01-23 12:55:52 +0000

    feat: PWA (WIP)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,19 @@ import { Toaster } from "@/components/ui/toaster";
 import { DrawingProvider } from "@/lib/contexts/drawing-context";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then(() => console.log("service worker registered"))
+        .catch((err) =>
+          console.error("service worker registration failed", err),
+        );
+    }
+  }, []);
   return (
     <DrawingProvider>
       <Toaster />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head></Head>
       <body className="antialiased">
         <Main />
         <NextScript />
+        <Script id="register-sw" strategy="afterInteractive">
+          {`
+                    if ('serviceWorker' in navigator) {
+                      window.addEventListener('load', function() {
+                        navigator.serviceWorker.register('/sw.js').then(
+                          function(registration) {
+                            console.log('Service Worker registration successful with scope: ', registration.scope);
+                          },
+                          function(err) {
+                            console.log('Service Worker registration failed: ', err);
+                          }
+                        );
+                      });
+                    }
+                  `}
+        </Script>
       </body>
     </Html>
   );
