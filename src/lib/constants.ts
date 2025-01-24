4a75c6cae67759e0bd5da0660d53770e2d73d48e
langford <langfordquarshie21@gmail.com>
2025-01-24 07:28:00 +0000

    feat(auth): sign in with google [wip]

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const ROUTES = {
+  auth: { signIn: "/auth/sign-in" },
+};
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/auth/login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-[#fff9ee]">
+      <LoginForm />
+    </div>
+  );
+}
